9843614295cc721ae9e63bd93e595e3f86bd0b94
Henry Nystrom <JanHenryNystrom@gmail.com>
2016-02-25 22:52:34 +0100

    Slight simplfication.

--- src/chash.erl
+++ src/chash.erl
@@ -33,7 +33,7 @@
 -export([jump/2]).
 
 -define(KSTEP, 2862933555777941757).
--define(JSTEP, 2147483648.0).
+-define(JSTEP, 2147483648).
 
 %% ===================================================================
 %% Library functions.
@@ -57,5 +57,5 @@ jump(J, B, _, Buckets) when J >= Buckets -> B;
 jump(J, _, Key, Buckets) ->
     <<Key0:64>> = <<(Key * ?KSTEP):64>>,
     Key1 = Key0 + 1,
-    J1 = trunc((J + 1) * (?JSTEP / float((Key1 bsr 33) + 1))),
+    J1 = trunc((J + 1) * (?JSTEP / ((Key1 bsr 33) + 1))),
     jump(J1, J, Key1, Buckets).
